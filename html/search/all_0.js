b15904dffee66e2b9d75eaada7cc6cb7a807809f
Doxygen CI <ci-arduino@invalid>
2023-03-11 03:04:08 +0000

    Deploy docs to GitHub Pages from commit 7845850a63
    Commit: 7845850a63a1a04a08f8d4c4ccca109cbd40f6c7
    GitHub Actions run: 4390284821

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_l3_g4200_d___unified.html#a666f9ea938feaae0cfb5079a523403a2',1,'L3G4200D_Unified']]]
+  ['begin',['begin',['../class_l3_g4200_d___unified.html#a311fe45c32e2b9e33e86681290807bab',1,'L3G4200D_Unified']]]
 ];
